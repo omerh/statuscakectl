fdb91824dfe2ebfb9248f4c373dbe736b81bac88
Pavel Voronov <pavel.voronov@paygee.com>
2022-04-06 10:20:14 +0200

    Add GetDetailedTestData for uptime test

--- statuscake/uptime.go
+++ statuscake/uptime.go
@@ -14,14 +14,59 @@ import (
 
 // UptimeCheckResult response from api
 type UptimeCheckResult struct {
-	TestID      int    `json:"TestID"`
-	Paused      bool   `json:"Paused"`
-	TestType    string `json:"TestType"`
-	WebsiteName string `json:"WebsiteName"`
-	WebsiteURL  string `json:"WebsiteURL"`
-	ContactID   int    `json:"ContactID"`
-	Status      string `json:"Status"`
-	Uptime      int    `json:"Uptime"`
+	TestID      int     `json:"TestID"`
+	Paused      bool    `json:"Paused"`
+	TestType    string  `json:"TestType"`
+	WebsiteName string  `json:"WebsiteName"`
+	WebsiteURL  string  `json:"WebsiteURL"`
+	ContactID   int     `json:"ContactID"`
+	Status      string  `json:"Status"`
+	Uptime      float64 `json:"Uptime"`
+}
+
+// UptimeCheckDetailed response from api
+type UptimeCheckDetailed struct {
+	Method        string `json:"Method"`
+	TestID        int    `json:"TestID"`
+	TestType      string `json:"TestType"`
+	Paused        bool   `json:"Paused"`
+	WebsiteName   string `json:"WebsiteName"`
+	URI           string `json:"URI"`
+	ContactGroup  string `json:"ContactGroup"`
+	ContactID     int    `json:"ContactID"`
+	ContactGroups []struct {
+		ID    int    `json:"ID"`
+		Name  string `json:"Name"`
+		Email string `json:"Email"`
+	} `json:"ContactGroups"`
+	Status           string   `json:"Status"`
+	Tags             []string `json:"Tags"`
+	Uptime           int      `json:"Uptime"`
+	CheckRate        int      `json:"CheckRate"`
+	Timeout          int      `json:"Timeout"`
+	LogoImage        string   `json:"LogoImage"`
+	Confirmation     string   `json:"Confirmation"`
+	FinalEndpoint    string   `json:"FinalEndpoint"`
+	WebsiteHost      string   `json:"WebsiteHost"`
+	NodeLocations    []string `json:"NodeLocations"`
+	FindString       string   `json:"FindString"`
+	DoNotFind        bool     `json:"DoNotFind"`
+	LastTested       string   `json:"LastTested"`
+	NextLocation     string   `json:"NextLocation"`
+	Processing       bool     `json:"Processing"`
+	ProcessingState  string   `json:"ProcessingState"`
+	ProcessingOn     string   `json:"ProcessingOn"`
+	DownTimes        string   `json:"DownTimes"`
+	TriggerRate      string   `json:"TriggerRate"`
+	Sensitive        bool     `json:"Sensitive"`
+	EnableSSLWarning bool     `json:"EnableSSLWarning"`
+	FollowRedirect   bool     `json:"FollowRedirect"`
+	Dnsip            string   `json:"DNSIP"`
+	DNSServer        string   `json:"DNSServer"`
+	CustomHeader     string   `json:"CustomHeader"`
+	PostRaw          string   `json:"PostRaw"`
+	UseJar           int      `json:"UseJar"`
+	StatusCodes      []string `json:"StatusCodes"`
 }
 
 // ListUptime listing all uptime checks in the account
@@ -141,4 +186,48 @@ func DeleteUptimeCheck(id int, api, user, key string) bool {
 		return false
 	}
 	return true
+}
+
+// GetDetailedTestData gets detailed uptime data for test
+func GetDetailedTestData(id int, api, user, key string) UptimeCheckDetailed {
+	client := &http.Client{}
+	request, _ := http.NewRequest("GET", api+"/API/Tests/Details/", nil)
+	// headers
+	request.Header.Add("Username", user)
+	request.Header.Add("API", key)
+	// params
+	q := request.URL.Query()
+	q.Add("TestID", strconv.Itoa(id))
+	request.URL.RawQuery = q.Encode()
+
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Println(err)
+		return UptimeCheckDetailed{}
+	}
+	defer resp.Body.Close()
+
+	// debug
+	// responseBody, _ := ioutil.ReadAll(resp.Body)
+	// log.Println(string(responseBody))
+
+	if resp.StatusCode != 200 {
+		message := helpers.ResolveStatusCode(resp.StatusCode)
+		log.Println(message)
+		return UptimeCheckDetailed{}
+	}
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var uptimeCheckData UptimeCheckDetailed
+	e := json.Unmarshal(responseBody, &uptimeCheckData)
+	if e != nil {
+		log.Println(e)
+		log.Println("Error: Failed to parse response body")
+	}
+
+	return uptimeCheckData
 }
